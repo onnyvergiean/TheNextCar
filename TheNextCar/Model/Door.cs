2ee6bd96753dac046f94c510857cf004d51aff00
onny <onny.s@students.amikom.ac.id>
2020-10-27 13:05:16 +0700

    Menambahkan Codingan pada Class Door

--- TheNextCar/Model/Door.cs
+++ TheNextCar/Model/Door.cs
@@ -6,5 +6,37 @@ namespace TheNextCar.Model
 {
     class Door
     {
+        private bool closed;
+        private bool locked;
+
+        public void close()
+        {
+            this.closed = true;
+        }
+
+        public void open()
+        {
+            this.closed = false;
+        }
+
+        public void activateLock()
+        {
+            this.locked = true;
+        }
+
+        public void unlock()
+        {
+            this.locked = false;
+        }
+
+        public bool isLocked()
+        {
+            return this.locked;
+        }
+
+        public bool isClosed()
+        {
+            return this.closed;
+        }
     }
 }
